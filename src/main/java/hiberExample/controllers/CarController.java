8c72189d03d607556d0acfda5d2f08b6681a4be7
vchernavin <chernavinv@hotmail.com>
2018-10-30 17:30:27 +0200

    Refactoring

--- src/main/java/hiberExample/controllers/CarController.java
+++ src/main/java/hiberExample/controllers/CarController.java
@@ -4,13 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,40 +25,40 @@ public class CarController {
 	@Autowired
 	private CarService carService;
 
-	@RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping
 	@ResponseStatus(value = HttpStatus.OK)
 	public @ResponseBody
 	List<Car> getAll() {
 		return carService.getAll();
 	}
 
-	@RequestMapping(value = "/filterByCompany", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping(value = "/filterByCompany")
 	@ResponseStatus(value = HttpStatus.OK)
 	public @ResponseBody
 	List<Car> getByCompany(String name) {
 		return carService.getByCompany(name);
 	}
 
-	@RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+	@PostMapping
 	@ResponseStatus(value = HttpStatus.OK)
 	public void create(@RequestBody Car car) {
 		carService.create(car);
 	}
 
-	@RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping(value = "/{id}")
 	@ResponseStatus(value = HttpStatus.OK)
 	public @ResponseBody
 	Car get(@PathVariable Long id) {
 		return carService.get(id);
 	}
 
-	@RequestMapping(method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
+	@PutMapping
 	@ResponseStatus(value = HttpStatus.OK)
 	public void update(@RequestBody Car car) {
 		carService.update(car);
 	}
 
-	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
+	@DeleteMapping(value = "/{id}")
 	@ResponseStatus(value = HttpStatus.OK)
 	public void delete(@PathVariable Long id) {
 		carService.delete(id);
--- src/main/java/hiberExample/controllers/DepartmentController.java
+++ src/main/java/hiberExample/controllers/DepartmentController.java
@@ -1,4 +1,62 @@
 package hiberExample.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import hiberExample.models.Department;
+import hiberExample.services.DepartmentService;
+
 public class DepartmentController {
+	@Autowired
+	private DepartmentService departmentService;
+
+	@GetMapping
+	@ResponseStatus(value = HttpStatus.OK)
+	public @ResponseBody
+	List<Department> getAll() {
+		return departmentService.getAll();
+	}
+
+	@GetMapping(value = "/{id}")
+	@ResponseStatus(value = HttpStatus.OK)
+	public @ResponseBody
+	Department get(@PathVariable Long id) {
+		return departmentService.get(id);
+	}
+
+	@GetMapping(value = "/filter")
+	@ResponseStatus(value = HttpStatus.OK)
+	public @ResponseBody
+	Department get(@RequestParam String name) {
+		return departmentService.get(name);
+	}
+
+	@PostMapping
+	@ResponseStatus(value = HttpStatus.OK)
+	public void create(@RequestBody Department department) {
+		departmentService.create(department);
+	}
+
+	@PutMapping
+	@ResponseStatus(value = HttpStatus.OK)
+	public void update(@RequestBody Department department) {
+		departmentService.update(department);
+	}
+
+	@DeleteMapping(value = "/{id}")
+	@ResponseStatus(value = HttpStatus.OK)
+	public void delete(@PathVariable Long id) {
+		departmentService.delete(id);
+	}
 }
