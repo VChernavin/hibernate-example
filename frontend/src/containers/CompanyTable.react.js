7e769c371d3a2b1cea9a1e60454b890aadcc7cba
O_Viacheslav Chernavin <vchernavin@luxoft.com>
2019-03-04 16:48:07 +0200

    Add expendable rows to CompanyTable

--- frontend/src/containers/CompanyTable.react.js
+++ frontend/src/containers/CompanyTable.react.js
@@ -10,6 +10,22 @@ const cellEditProp = {
 
 const OBJECT_TYPE = 'company';
 
+class BSTable extends React.Component {
+  render() {
+    if (this.props.data) {
+      return (
+        <BootstrapTable data={this.props.data} >
+          <TableHeaderColumn width='100' dataField='id' editable={false} isKey export={false} >ID</TableHeaderColumn >
+          <TableHeaderColumn width='400' dataField='name' >Name</TableHeaderColumn >
+          <TableHeaderColumn dataField='street' >Street</TableHeaderColumn >
+          <TableHeaderColumn width='100' dataField='houseNumber' >House Number</TableHeaderColumn >
+          <TableHeaderColumn width='400' dataField='zipCode' >ZIP Code</TableHeaderColumn >
+        </BootstrapTable >);
+    } else {
+      return (<p >?</p >);
+    }
+  }
+}
 
 export default class CompanyTable extends React.Component {
   constructor(props) {
@@ -62,28 +78,55 @@ export default class CompanyTable extends React.Component {
     const options = {
       onAddRow: this.onAddRow,
       onDeleteRow: this.onDeleteRow,
-      onCellEdit: this.onCellEdit
+      onCellEdit: this.onCellEdit,
+      expandRowBgColor: 'MediumAquamarine',
     };
 
-    // const paramNames = [];
-    //
-    // const resultList = [];
-    //
-    // const createObject = (item, index) => {
-    //   if (index === 0) {
-    //     item.forEach(propenty => paramNames.push(propenty));
-    //   } else if (item[0] !== "") {
-    //     const tmpObj = {};
-    //     item.forEach((item, index) => {
-    //       tmpObj[paramNames[index]] = item;
-    //       // console.log(tmpObj);
-    //       resultList.push(tmpObj);
-    //     })
-    //   }
-    // };
+    const selectRow = {
+      mode: 'checkbox',
+      clickToSelect: true,
+      clickToExpand: true
+    };
+
+    const expandColumnComponent = ({isExpandableRow, isExpanded}) => {
+      let content = '';
+
+      if (isExpandableRow) {
+        content = (isExpanded ? '(-)' : '(+)');
+      } else {
+        content = ' ';
+      }
+      return (
+        <div > {content} </div >
+      );
+    };
+
+    const expandColumnOptions = {
+      expandColumnVisible: true,
+      expandColumnComponent,
+      columnWidth: 50
+    };
+
 
     const handleForce = data => {
       data.forEach(obj => onAdd(OBJECT_TYPE, this.setStateHandler, obj));
+    };
+
+    const isExpandableRow = row => row.offices.length > 0;
+
+    const expandComponent = row => {
+      let offices = row.offices.map(office => {
+        return {
+          id: office.id,
+          name: office.name,
+          street: office.address.street,
+          houseNumber: office.address.houseNumber,
+          zipCode: office.address.zipCode,
+        }
+      });
+      return (
+        <BSTable data={offices} />
+      );
     };
 
     return (
@@ -93,13 +136,16 @@ export default class CompanyTable extends React.Component {
         <BootstrapTable data={this.state.companies}
                         remote={true}
                         deleteRow={true}
-                        selectRow={{mode: 'checkbox'}}
+                        selectRow={selectRow}
                         cellEdit={cellEditProp}
+                        expandableRow={isExpandableRow}
+                        expandComponent={expandComponent}
+                        expandColumnOptions={expandColumnOptions}
                         options={options}
                         insertRow={true}
                         exportCSV={true}
                         striped hover condensed >
-          <TableHeaderColumn width='100' dataField='id' editable={false} isKey export={false} >ID</TableHeaderColumn >
+          <TableHeaderColumn width='100' dataField='id' editable={false} isKey export={false} hiddenOnInsert>ID</TableHeaderColumn >
           <TableHeaderColumn dataField='name' >Name</TableHeaderColumn >
         </BootstrapTable >
       </div >
